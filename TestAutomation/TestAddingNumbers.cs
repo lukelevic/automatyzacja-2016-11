a89156c1991506eec73c1e33fa2040b9b9d7b966
Rafał Markowicz <rafal.markowicz@codesprinters.com>
2016-09-21 16:09:08 +0200

    test seqeuential

---
+++ TestAutomation/TestAddingNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestAutomation
+{
+    class TestAddingNumbers
+    {
+        [Test, Sequential]
+        public void shouldConverToIntAndAdd([Values("1", "2", "3")] string stringOne, [Values("2", "3", "7")] string stringTwo, [Values(3, 5, 10)] int expResult)
+        {
+            int digitOne = Convert.ToInt32(stringOne);
+            int digitTwo = Convert.ToInt32(stringTwo);
+            int calcResult = digitOne + digitTwo;
+            Assert.AreEqual(calcResult, expResult);
+        }
+    }
+}
