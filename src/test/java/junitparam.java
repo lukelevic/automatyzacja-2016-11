25c3b1c4c6a7a04553e32a1791f198d3db5c1f94
sdabrowski79 <sebastian.dabrowski79@gmail.com>
2016-09-21 16:16:52 +0200

    trzeci

---
+++ src/test/java/junitparam.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2016-09-21.
+ */
+import org.junit.Test;
+import junitparams.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+
+@RunWith(JUnitParamsRunner.class)
+public class junitparam {
+
+
+    @Test
+    @Parameters({"1,2,3", "2,3,5", "3,7,10"})
+    public void adding (String p1, String p2, int p3) {
+
+        int pp1 = Integer.parseInt(p1);
+        int pp2 = Integer.parseInt(p2);
+        Assert.assertEquals(pp1+pp2, p3 );
+
+}
+}
