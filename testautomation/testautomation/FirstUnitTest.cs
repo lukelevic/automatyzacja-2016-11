14e58544c65b71581a2df5a0da4f1efaa144ed96
Michalgrc <michal.grcc@gmail.com>
2016-09-21 15:12:48 +0200

    new commit

---
+++ testautomation/testautomation/FirstUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace testautomation
+{
+    class FirstUnitTest
+    {
+        [SetUp]
+        public void InitialTest()
+        {
+            DayOffWeek.Add("Sunday");
+        }
+        List<string> DayOffWeek = new List<string>();
+        // [SetUp]
+        // [TearDown]
+        // [TestFixtureSetup]
+        // [TestFixtureTearDown]
+
+        [Test]
+        public void testOne()
+        {
+
+           
+            DayOffWeek.Add("Monday");
+            Console.WriteLine("test one");
+            Assert.AreEqual(2, DayOffWeek.Count());
+                 
+           
+           
+
+
+        }
+        [Test]
+        public void testTwo()
+        {
+            DayOffWeek.Remove("Sunday");
+            Console.WriteLine("test two");
+            Assert.AreEqual(2, DayOffWeek.Count());
+        }
+       
+
+    }
+}
