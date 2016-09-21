adc97ce80a8963bc343f1c4268b852df4f765d0f
alewandowski <lewandowskiadam@tlen.pl>
2016-09-21 16:08:00 +0200

    adding sequential tests

--- TestAutomation/FristUnitTest.cs
+++ TestAutomation/FristUnitTest.cs
@@ -39,6 +39,27 @@ namespace TestAutomation
             Assert.AreEqual(result, false);
         }
 
+
+
+
+        [Test, Sequential]
+        public void SequentialTest(
+            [Values("1", "2", "10")] string s1,
+            [Values("5", "6", "20")] string s2,
+            [Values(6, 8, 30)] int x
+            )
+        {
+            int ints1;
+            int.TryParse(s1, out ints1);
+
+            int ints2;
+            int.TryParse(s2, out ints2);
+
+            var result = ints1 + ints2;
+
+            Assert.AreEqual(result, x);
+        }
+
         [OneTimeTearDown]
         public void EndOfTests()
         {
