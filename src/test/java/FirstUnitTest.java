3e7c4cbe1a74569333cb4bb5a61462919b219ffc
Maciej Teodorowski <maciej.teodorowski@gmail.com>
2016-09-21 16:14:13 +0200

    tests with junitparams

--- src/test/java/FirstUnitTest.java
+++ src/test/java/FirstUnitTest.java
@@ -2,13 +2,16 @@
  * Created by Administrator on 2016-09-21.
  */
 
+import junitparams.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(JUnitParamsRunner.class)
 public class FirstUnitTest {
     private static final String DAY_NAME = "Monday";
     private List<String> week = new ArrayList<String>();
@@ -34,5 +37,13 @@ public class FirstUnitTest {
         week.remove(DAY_NAME);
         Assert.assertEquals(0, week.size());
         Assert.assertFalse(week.contains(DAY_NAME));
+    }
+
+    @Test
+    @Parameters({"1,2,3", "2,3,5", "3,7,10"})
+    public void addStrings(String p1, String p2, int p3) {
+        int _p1 = Integer.parseInt(p1);
+        int _p2 = Integer.parseInt(p2);
+        Assert.assertEquals(p3, _p1+_p2);
     }
 }
