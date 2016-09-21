63bf65ae966f0eb4575b3f662f6f365c5d409b25
Rafał Markowicz <rafal.markowicz@codesprinters.com>
2016-09-21 15:13:58 +0200

    testy tear down i setup

---
+++ TestAutomation/FirstUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestAutomation
+{
+    class FirstUnitTest
+    {
+        List<string> listOfDays;
+        [TestFixtureSetUp]
+        public void testFixtureSetup()
+        {
+            this.listOfDays = new List<string>();
+        }
+
+        [SetUp]
+        public void setUp()
+        {
+            listOfDays.Add("Sunday");
+        }
+        [TearDown]
+        public void tearDown()
+        {
+            listOfDays.Remove("Sunday");
+        }
+        //[TestFixtureSetUp]
+        //[TestFixtureTearDown]
+
+        [Test]
+        public void shouldAddMonday()
+        {
+            listOfDays.Add("Monday");
+            Assert.True(listOfDays.Contains("Monday"));
+        }
+        [Test]
+        public void shouldRemoveMonday()
+        {
+            listOfDays.Remove("Monday");
+            Assert.False(listOfDays.Contains("Monday"));
+            Assert.That(listOfDays.Count, Is.EqualTo(1));
+        }
+
+    }
+}
