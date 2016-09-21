4a49b06d865d10f7c623ce9a231787da52000654
luke levic <lukasz.lewicki@rst.com.pl>
2016-09-21 16:16:24 +0200

    third test

--- src/test/java/FirstUnitTest.java
+++ src/test/java/FirstUnitTest.java
@@ -2,11 +2,15 @@
  * Created by Administrator on 2016-09-21.
  */
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.*;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class FirstUnitTest {
 
 //    @Before
@@ -24,14 +28,24 @@ public class FirstUnitTest {
     @Test
     public void testFirst() {
         System.out.println("testFirst");
-        Assert.assertEquals(list.contains("element"), true);
+        assertEquals(list.contains("element"), true);
     }
 
     @Test
     public void testSecond() {
         System.out.println("testSecond");
         list.clear();
-        Assert.assertEquals(list.contains("element"), false);
+        assertEquals(list.contains("element"), false);
+    }
+
+    @Test
+    @Parameters({"1,2,3","2,3,5"})
+    public void testSum(String A, String B, int C) {
+        System.out.println("testThird");
+        int _A = Integer.parseInt(A);
+        int _B = Integer.parseInt(B);
+        int suma = (_A+_B);
+        assertEquals(suma, C);
     }
 
 
