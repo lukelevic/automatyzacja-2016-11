cf2af2bc7eae6b2ce70a077856f1e39adb4cf933
krayevsky <krayevsky@gmail.com>
2016-09-21 15:13:15 +0200

    first unitTests

---
+++ TestAutomation/firstUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+
+namespace TestAutomation
+{
+    class firstUnitTest
+    {
+
+        List<string> days;
+
+        [SetUp]
+        public void SetUp()
+        {
+        days = new List<string>();
+        days.Add("Monday");
+        days.Add("Tuesday");
+        days.Add("Wednesday");
+        }
+
+        [Test]
+        public void shouldItemBeAdded()
+        {
+            bool actual = days.Contains("Monday");
+            Assert.AreEqual(true, actual);
+        }
+
+        [Test]
+        public void shouldItemBeRemoved()
+        {
+
+            days.Remove("Tuesday");
+            bool actual = days.Contains("Tuesday");
+
+            Assert.AreEqual(false, actual);
+            Assert.AreEqual (2, days.Count());
+        }
+
+
+    }
+}
+
+// [TearDown]
+// [TestFixtureSetUp]
+// [TestFixtureTearDown]
