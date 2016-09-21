6af215cc5d336826f02e14c0505dfaab5871e702
andrzejm <andrzejmorawski334@gmail.com>
2016-09-21 15:11:15 +0200

    test message

---
+++ testautomation/testautomation/FirstUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace testautomation
+{
+    class FirstUnitTest
+    {
+
+        List<string> Lista = new List<string>();
+
+        [SetUp] //przed kazdym testem
+        public void Start()
+        {
+            Lista.Add("elem 1");
+        }
+        //[TearDown] po kazdym tescie
+        //[TestFixtureSetUp] przed wszystkimi testami
+        //[TestFixtureTearDown] po wszystkich testach
+
+        // atrybut wymagany zeby bylo wiadomo ze to test
+        [Test]
+        public void ShouldAddElementToList()
+        {
+            Lista.Add("elem 2");
+            // wykonac akcje
+            Console.WriteLine("Test one text: {0}", Lista[0]);
+            // sprawdzic wynik
+            Assert.AreEqual(2, Lista.Count());
+        }
+
+        [Test]
+        public void ShouldRemoveElementFromList()
+        {
+            Lista.Remove("elem 1");
+            // wykonac akcje
+            Console.WriteLine("Test two text");
+            // sprawdzic wynik
+            Assert.AreEqual(2, Lista.Count());
+        }
+    }
+}
