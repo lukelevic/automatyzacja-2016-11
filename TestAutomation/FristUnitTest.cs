304752107bff5707e129949d59245190cf376647
alewandowski <lewandowskiadam@tlen.pl>
2016-09-21 15:17:43 +0200

    adding setup/teardown

---
+++ TestAutomation/FristUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace TestAutomation
+{
+    public class FirstUnitTest
+    {
+        public List<string> list;
+
+        [OneTimeSetUp]
+        public void InitializeTestClass()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        [SetUp]
+        public void InitializeParticularTest()
+        {
+            list = new List<string>() { "A", "B", "C" };
+        }
+        
+        [Test]
+        public void IsOnList()
+        {
+            var result = list.Contains("C");
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void IsRemovedFromList()
+        {
+            list.Remove("C");
+            var result = list.Contains("C");
+            Assert.AreEqual(result, false);
+        }
+
+        [OneTimeTearDown]
+        public void EndOfTests()
+        {
+            Console.WriteLine("Bye");
+        }
+    }
+    
+}
