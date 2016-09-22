64231a499b27b6281baf7d1e198aa7144e8d4908
jakubgorniak <jakub.gorniak@poczta.fm>
2016-09-22 15:17:47 +0200

    drugi commit3

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,11 +1,7 @@
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
@@ -18,44 +14,56 @@ public class SeleniumTest {
     public void setUp() throws Exception {
 
 
-
+        //given
        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 
         driver = new ChromeDriver(); //("C:\\chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-
-       // driver = new ChromeDriver();
-
-        // driver = new FirefoxDriver();
         baseUrl = "https://automatyzacja2016.wordpress.com/";
-        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
         driver.get(baseUrl + "wp-admin");
         driver.manage().window().maximize();
+
+
+
     }
 
     @Test
-    public void testSeleniumTest() throws Exception {
-        driver.findElement(By.id("user_login")).clear();
-        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-        driver.findElement(By.id("user_pass")).clear();
-        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
-        driver.findElement(By.id("wp-submit")).click();
+    public void ShouldLoginWithCredential() throws Exception {
 
-        driver.findElement(By.xpath(".//*[@id='wp-admin-bar-ab-new-post']/a")).click();
-            Thread.sleep(3000);
-        driver.findElement(By.xpath(".//*[@id='primary']/div/div[2]/div[1]/div[2]/div[3]/div/textarea")).sendKeys("jakubg");
-            Thread.sleep(3000);
-        driver.findElement(By.xpath(" .//*[@id='primary']/div/div[2]/div[2]/div[2]/div[1]/div[3]/div/button[1]")).click();
-            Thread.sleep(1000);
-        driver.findElement(By.xpath(".//*[@id='primary']/div/div[2]/div[1]/div[2]/div[2]/div/div/a")).click();
-            Thread.sleep(3000);
+
+
+
+        //when
+        //TODO login(user,password)
+        insert(By.id("user_login"), "szkolenieautomatyzacja");
+        insert(By.id("user_pass"), "QW12qw12");
+        click(By.id("wp-submit"));
+
+        //than
+
+        click(By.xpath(".//*[@id='wp-admin-bar-ab-new-post']/a"));
+        Thread.sleep(3000);
+        xpathSendKey(By.xpath(".//*[@id='primary']/div/div[2]/div[1]/div[2]/div[3]/div/textarea"));
+        Thread.sleep(3000);
+        click(By.xpath(" .//*[@id='primary']/div/div[2]/div[2]/div[2]/div[1]/div[3]/div/button[1]"));
+        Thread.sleep(1000);
+        click(By.xpath(".//*[@id='primary']/div/div[2]/div[1]/div[2]/div[2]/div/div/a"));
+        Thread.sleep(3000);
         Assert.assertTrue("jakubg", driver.getPageSource().contains("jakubg"));
         System.out.println("Jest git!!!!!!!!!!!!!!!!!");
 
+    }
 
+    private void xpathSendKey(By xpath) {
+        driver.findElement(xpath).sendKeys("jakubg");
+    }
 
+    private void click(By xpath) {
+        driver.findElement(xpath).click();
+    }
 
+    private void insert(By login, String text) {
+        driver.findElement(login).clear();
+        driver.findElement(login).sendKeys(text);
     }
 
     @After
