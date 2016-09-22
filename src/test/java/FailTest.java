7f7c81acb6e7cc4a153388de64c37ab7e354382a
michaljanuszs <stalmach.m@gmail.com>
2016-09-22 10:10:31 +0200

    test zzawsze failowany

---
+++ src/test/java/FailTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by MichalJanuszS 2016-09-21.
+ */
+import org.junit.*;
+
+
+public class FailTest {
+    @Test
+    public void testZlegoTestu(){
+        Assert.assertEquals(false, true);
+    }
+}
