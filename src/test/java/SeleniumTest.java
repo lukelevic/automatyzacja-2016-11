7f643479e1a9b8a39b1a788dcb81f240953ee83c
Grzegorz Dobiecki <g.dobiecki@gmail.com>
2016-09-22 11:17:36 +0200

    add chromedriver

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,72 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://automatyzacja2016.wordpress.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testCase() throws Exception {
+        driver.get(baseUrl + "/wp-login.php");
+        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
+        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
+        driver.findElement(By.id("wp-submit")).click();
+        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
+        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
