d19cdfa81a939cc8138c69998934f2e760f7f931
luke levic <lukasz.lewicki@rst.com.pl>
2016-09-22 16:49:31 +0200

    refactor

---
+++ src/test/java/AbstactClass.java
@@ -0,0 +1,86 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
+
+public class AbstactClass  {
+    public WebDriver driver;
+    public String baseUrl;
+    public boolean acceptNextAlert = true;
+    public StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\GIT\\automatyzacja-2016-11\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://automatyzacja2016.wordpress.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    public void  logIn() {
+        driver.get(baseUrl + "/wp-login.php");
+        driver.findElement(By.id("user_login")).clear();
+        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
+        driver.findElement(By.id("user_pass")).clear();
+        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
+        driver.findElement(By.id("wp-submit")).click();
+    }
+
+    public void goToNewPost() {
+        driver.findElement(By.xpath("//div[@class='wp-menu-image dashicons-before dashicons-admin-post']")).click();
+        driver.findElement(By.xpath("//div[@class='wrap']//a[text()='Dodaj nowy']")).click();
+    }
+
+    public void publishNewPost(String title, String content, Integer random) {
+        driver.findElement(By.xpath("//div[@id='titlediv']//input[@id='title']")).sendKeys(title+" "+random);
+        driver.findElement(By.xpath("//textarea[@class='wp-editor-area']")).sendKeys(content);
+        driver.findElement(By.xpath("//input[@id='publish']")).click();
+    }
+
+    public int getRandom() {
+        int randNum = (int) (Math.random() * 1000);
+        return randNum;
+    }
+
+    public void goToAllPostView() {
+        driver.findElement(By.xpath("//li[@id='menu-posts']//a[@class='wp-first-item current']")).click();
+    }
+
+    public void logOut() {
+        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
+        w8("//button[contains(@class, 'sign-out')]").click();
+    }
+
+    public void goToHomePage() {
+        driver.get(baseUrl);
+    }
+
+    public void goToPostView(String title) {
+        driver.findElement(By.xpath("//div[@id='content']//a[contains(text(),'"+title+"')]")).click();
+    }
+
+    public WebElement w8(String path) {
+        WebElement w8 = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath(path)));
+        return w8;
+    }
+
+
+}
--- src/test/java/SeleniumIDETest.java
+++ src/test/java/SeleniumIDETest.java
@@ -1,25 +1,12 @@
-package com.example.tests;
-
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumIDETest {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+public class SeleniumIDETest extends AbstactClass{
+    private String content = "treść posta";
+    int rand = getRandom();
+    private String title = "nowy pościk";
 
-    @Before
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\GIT\\automatyzacja-2016-11\\src\\main\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
-        baseUrl = "https://automatyzacja2016.wordpress.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
+
 
 //    @Test
 //    public void testWordpress() throws Exception {
@@ -35,65 +22,50 @@ public class SeleniumIDETest {
 
     @Test
     public void testAddPostAndCheck() throws Exception {
-        int randNum = (int) (Math.random() * 1000);
-        driver.get(baseUrl + "/wp-login.php");
-        driver.findElement(By.id("user_login")).clear();
-        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-        driver.findElement(By.id("user_pass")).clear();
-        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
-        driver.findElement(By.id("wp-submit")).click();
-        driver.findElement(By.xpath("//div[@class='wp-menu-image dashicons-before dashicons-admin-post']")).click();
-        driver.findElement(By.xpath("//div[@class='wrap']//a[text()='Dodaj nowy']")).click();
-        driver.findElement(By.xpath("//div[@id='titlediv']//input[@id='title']")).sendKeys("nowy pościk "+randNum);
-        driver.findElement(By.xpath("//textarea[@class='wp-editor-area']")).sendKeys("nowa tresc");
-        driver.findElement(By.xpath("//input[@id='publish']")).click();
-        driver.findElement(By.xpath("//li[@id='menu-posts']//a[@class='wp-first-item current']")).click();
-        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
-        driver.findElement(By.xpath("//button[contains(@class, 'sign-out')]")).click();
-        driver.get(baseUrl);
-        driver.findElement(By.xpath("//div[@id='content']//a[contains(text(),'nowy pościk')]")).click();
-        assertTrue(driver.getTitle().contains("nowy pościk "+randNum));
+        logIn();
+        goToNewPost();
+        publishNewPost(title, content, rand);
+        goToAllPostView();
+        logOut();
+        goToHomePage();
+        goToPostView(title);
+        assertTrue(driver.getTitle().contains(title+" "+rand));
     }
 
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
 
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+//    private boolean isElementPresent(By by) {
+//        try {
+//            driver.findElement(by);
+//            return true;
+//        } catch (NoSuchElementException e) {
+//            return false;
+//        }
+//    }
+//
+//    private boolean isAlertPresent() {
+//        try {
+//            driver.switchTo().alert();
+//            return true;
+//        } catch (NoAlertPresentException e) {
+//            return false;
+//        }
+//    }
+//
+//    private String closeAlertAndGetItsText() {
+//        try {
+//            Alert alert = driver.switchTo().alert();
+//            String alertText = alert.getText();
+//            if (acceptNextAlert) {
+//                alert.accept();
+//            } else {
+//                alert.dismiss();
+//            }
+//            return alertText;
+//        } finally {
+//            acceptNextAlert = true;
+//        }
+//    }
 
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
+
 }
