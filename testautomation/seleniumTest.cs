f179dc1689a848e24383b41d889118e7a65f93a0
AlicjaKedziora <alicja.kedziora@op.pl>
2016-09-22 16:56:59 +0200

    lepiej nie bedzie

--- testautomation/seleniumTest.cs
+++ testautomation/seleniumTest.cs
@@ -13,44 +13,29 @@ namespace SeleniumTests
     [TestFixture]
     public class seleniumTest : Scenario
     {
-               
-
         [Test]
         public void ShouldBePossibleToAddNewPost()
-        {
-
+        {   //given
             LogIn();
-
+            //when
             AddNewPost();
-
+            //then
             AssertNewPostAdded();
-
-        }
-
+         }
         private void AssertNewPostAdded()
-        {
+         {
             Click(By.XPath("//a[contains(.,'https://automatyzacja2016.wordpress.com/2016/09/22/nowy-tytul-ala')]"));
-
-
             LogOut();
             Assert.Pass("przeszlo");
-        }
-
+         }
         private void AddNewPost()
-        {
+         {
             Click(By.LinkText("Wpisy"));
-
             Click((By.LinkText("Dodaj nowy")));
-
             Insert(By.Name("post_title"), "nowy tytul ala" + ' ' + DateTime.Now);
-
-
             Click(By.Id("content-html"));
             Insert(By.Id("content"), "raz dwa trzy");
             Click(By.Id("publish"));
-        }
-
-      
-
-    }
+         }
+     }
 }
