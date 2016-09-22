6846751b88ecdf2d517d8049813e50c5a532b8d2
Lukasz F <fundakowski@gmail.com>
2016-09-22 09:58:23 +0200

    kolejny test

--- src/test/java/FirstUnitTest.java
+++ src/test/java/FirstUnitTest.java
@@ -51,12 +51,21 @@ public class FirstUnitTest {
     }
 
     @Test
-    @Parameters({"1,2,3","2,3,5", "3,7,10"})
+    @Parameters({"1,2,3","2,3,5", "3,7,10", "10,5,15", "20,30,50"})
     public void testTwo(String s1, String s2, String s3) {
         int a = Integer.parseInt(s1);
         int b = Integer.parseInt(s2);
         int c = Integer.parseInt(s3);
 
         Assert.assertEquals(a+b,c);
+    }
+    @Test
+    @Parameters({"5,2,3","8,3,5", "17,7,10"})
+    public void testOdejmowanie(String s1, String s2, String s3) {
+        int a = Integer.parseInt(s1);
+        int b = Integer.parseInt(s2);
+        int c = Integer.parseInt(s3);
+
+        Assert.assertEquals(a-b,c);
     }
 }
