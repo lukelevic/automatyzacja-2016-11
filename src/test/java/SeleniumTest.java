2069a13c872dd61a95bb3c5402144808e907f661
Grzegorz Dobiecki <g.dobiecki@gmail.com>
2016-09-22 13:01:08 +0200

    add a test

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -15,17 +17,33 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         baseUrl = "https://automatyzacja2016.wordpress.com/";
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void testCase() throws Exception {
-        driver.get(baseUrl + "/wp-login.php");
+        Date date= new java.util.Date();
+        String postTitle = "new post" + date;
+        driver.get(baseUrl + "wp-login.php");
         driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
         driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
         driver.findElement(By.id("wp-submit")).click();
+        driver.get(baseUrl + "wp-admin/post-new.php");
+        driver.findElement(By.id("title")).sendKeys(postTitle);
+        driver.findElement(By.id("content")).sendKeys("new message bla bla bla bla bla bla bla bla");
+        driver.findElement(By.id("publish")).click();
         driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
         driver.findElement(By.xpath("//*[@id=\"wp-admin-bar-user-info\"]/div/form/button")).click();
+        driver.get(baseUrl);
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + postTitle.substring(0, postTitle.length() - 5) + "')]"));
+
+        driver.navigate().refresh();
+        Assert.assertTrue("Text " + postTitle + " not found!", list.size() > 0);
     }
 
     @After
