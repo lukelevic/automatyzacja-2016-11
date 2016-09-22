318c982483a2d3a7375ee95b45b1a118c6aa6acd
Lukasz F <fundakowski@gmail.com>
2016-09-22 15:31:25 +0200

    refactoring 1

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Administrator on 2016-09-22.
+ */
+public class BaseTest {
+    protected WebDriver driver;
+    protected String baseUrl;
+
+
+    protected boolean acceptNextAlert = true;
+    protected StringBuffer verificationErrors = new StringBuffer();
+
+    public void open(String url) {
+        driver.get(baseUrl+url);
+
+    }
+
+    public void login(String user, String password) {
+        driver.findElement(By.id("user_login")).clear();
+        driver.findElement(By.id("user_login")).sendKeys(user);
+        driver.findElement(By.id("user_pass")).clear();
+        driver.findElement(By.id("user_pass")).sendKeys(password);
+        driver.findElement(By.id("wp-submit")).click();
+    }
+
+    public void logout() {
+        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button.ab-sign-out")));
+
+        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
+    }
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -13,11 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.support.ui.Select;
 
-public class SeleniumTest {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+public class SeleniumTest extends BaseTest {
+//    private WebDriver driver;
+
 
     @Before
     public void setUp() throws Exception {
@@ -29,15 +27,13 @@ public class SeleniumTest {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-//    @Test
-//    public void testSelenium() throws Exception {
-//        driver.get(baseUrl + "/wp-admin");
-//        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-//        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
-//        driver.findElement(By.id("wp-submit")).click();
-//        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
-//        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
-//    }
+    @Test
+    public void shouldLogIntoPage() throws Exception {
+        driver.get(baseUrl + "/wp-admin");
+        login("szkolenieautomatyzacja", "QW12qw12");
+        logout();
+//        Assert.assertEquals(driver.getTitle(),"Obserwowanie");
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -49,25 +45,15 @@ public class SeleniumTest {
     }
 
     @Test
-    public void testAddNewPost() {
-        // login
-        driver.get(baseUrl + "/wp-admin");
-        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
-        driver.findElement(By.id("wp-submit")).click();
-
-        // add new post
-        driver.findElement(By.xpath(".//*[@id='menu-posts']/a/div[3]")).click();
-        driver.findElement(By.xpath(".//*[@id='menu-posts']/ul/li[3]/a")).click();
-
+    public void ShouldAddNewPost() {
         Random randomId = new Random();
         String title = "Moj nowy post"+String.valueOf(randomId.nextInt(1000000));
         String post ="Test Lukasz"+String.valueOf(randomId.nextInt(1000000));
 
-        driver.findElement(By.xpath(".//*[@id='title']")).sendKeys(title);
-        driver.findElement(By.xpath(".//*[@id='content-html']")).click();
-        driver.findElement(By.xpath(".//*[@id='content']")).sendKeys(post);
-        driver.findElement(By.xpath(".//*[@id='publish']")).click();
+        open("/wp-admin");
+        login("szkolenieautomatyzacja", "QW12qw12");
+
+        addNewPost(title, post);
 
         try {
             Thread.sleep(1000);
@@ -75,17 +61,10 @@ public class SeleniumTest {
             e.printStackTrace();
         }
 
-        //log out
-        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
+        logout();
 
         //check if post is added
-        driver.get("https://automatyzacja2016.wordpress.com/");
+        open("");
 
         try {
             Thread.sleep(5000);
@@ -96,36 +75,14 @@ public class SeleniumTest {
         Assert.assertTrue("Nie ma postu o tresci: "+post,driver.getPageSource().contains(post));
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+    private void addNewPost(String title, String post) {
+        driver.findElement(By.xpath(".//*[@id='menu-posts']/a/div[3]")).click();
+        driver.findElement(By.xpath(".//*[@id='menu-posts']/ul/li[3]/a")).click();
+        driver.findElement(By.xpath(".//*[@id='title']")).sendKeys(title);
+        driver.findElement(By.xpath(".//*[@id='content-html']")).click();
+        driver.findElement(By.xpath(".//*[@id='content']")).sendKeys(post);
+        driver.findElement(By.xpath(".//*[@id='publish']")).click();
     }
 
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
