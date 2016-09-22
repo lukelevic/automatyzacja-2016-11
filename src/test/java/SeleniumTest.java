047460c8e454b055bc19dcca53434921c301adf6
Lukasz F <fundakowski@gmail.com>
2016-09-22 13:03:06 +0200

    Selenium test poprawiony

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,5 +1,6 @@
 //package com.example.tests;
 
+import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
@@ -7,6 +8,8 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.support.ui.Select;
 
@@ -21,19 +24,20 @@ public class SeleniumTest {
 
 //        driver = new ChromeDriver(@"C:\Workspace\automatyzacja-2016-11\src\main\resources");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         baseUrl = "https://automatyzacja2016.wordpress.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void testSelenium() throws Exception {
-        driver.get(baseUrl + "/wp-admin");
-        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
-        driver.findElement(By.id("wp-submit")).click();
-        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
-        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
-    }
+//    @Test
+//    public void testSelenium() throws Exception {
+//        driver.get(baseUrl + "/wp-admin");
+//        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
+//        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
+//        driver.findElement(By.id("wp-submit")).click();
+//        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
+//        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
+//    }
 
     @After
     public void tearDown() throws Exception {
@@ -42,6 +46,49 @@ public class SeleniumTest {
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
         }
+    }
+
+    @Test
+    public void testAddNewPost() {
+        // login
+        driver.get(baseUrl + "/wp-admin");
+        driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
+        driver.findElement(By.id("user_pass")).sendKeys("QW12qw12");
+        driver.findElement(By.id("wp-submit")).click();
+
+        // add new post
+        driver.findElement(By.xpath(".//*[@id='menu-posts']/a/div[3]")).click();
+        driver.findElement(By.xpath(".//*[@id='menu-posts']/ul/li[3]/a")).click();
+
+        Random randomId = new Random();
+        String title = "Moj nowy post"+String.valueOf(randomId.nextInt(1000000));
+        String post ="Test Lukasz"+String.valueOf(randomId.nextInt(1000000));
+
+        driver.findElement(By.xpath(".//*[@id='title']")).sendKeys(title);
+        driver.findElement(By.xpath(".//*[@id='content-html']")).click();
+        driver.findElement(By.xpath(".//*[@id='content']")).sendKeys(post.toString());
+        driver.findElement(By.xpath(".//*[@id='publish']")).click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //log out
+        driver.findElement(By.cssSelector("img.avatar.avatar-32")).click();
+        driver.findElement(By.cssSelector("button.ab-sign-out")).click();
+
+        //check if post is added
+        driver.get("https://automatyzacja2016.wordpress.com/");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue("Nie ma postu o tytule: "+title.toString(),driver.getPageSource().contains(title.toString()));
+        Assert.assertTrue("Nie ma postu o tresci: "+post.toString(),driver.getPageSource().contains(post.toString()));
     }
 
     private boolean isElementPresent(By by) {
