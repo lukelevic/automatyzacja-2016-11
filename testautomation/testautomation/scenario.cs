84df1ebe6e4437a4b55a0b9eaeb3813dbb0a618f
MarekWegrzyn <marek.wegrzyn86@gmail.com>
2016-09-22 16:36:49 +0200

    test6

---
+++ testautomation/testautomation/scenario.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    public class Scenario
+    {
+        protected IWebDriver driver;
+        protected String baseURL;
+        protected StringBuilder verificationErrors;
+        protected bool acceptNextAlert = false;
+        protected WebDriverWait wait;
+        [SetUp]
+        public void Setup()
+        {
+            //driver = new FirefoxDriver();
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            baseURL = "https://automatyzacja2016.wordpress.com/";
+            verificationErrors = new StringBuilder();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        protected void LoginIn(String login, String password)
+        {
+            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautomatyzacja2016.wordpress.com%2Fwp-admin%2F&reauth=1");
+            driver.FindElement(By.Id("user_login")).Clear();
+            driver.FindElement(By.Id("user_login")).SendKeys(login);
+            driver.FindElement(By.Id("user_pass")).Clear();
+            driver.FindElement(By.Id("user_pass")).SendKeys(password);
+            driver.FindElement(By.Id("wp-submit")).Click();
+        }
+
+        protected void AddPost(String text)
+        {
+            driver.FindElement(By.Id("wp-admin-bar-ab-new-post")).Click();
+            driver.FindElement(By.ClassName("editor-title__input")).SendKeys(text);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("editor-ground-control__publish-button")));
+            driver.FindElement(By.ClassName("editor-ground-control__publish-button")).Click();
+        }
+
+        protected void LogOut()
+        {
+            Thread.Sleep(2000);
+            driver.FindElement(By.ClassName("masterbar__item-me")).Click();
+            driver.FindElement(By.ClassName("me-sidebar__signout-button")).Click();
+        }
+
+        
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
--- testautomation/testautomation/selenium_test.cs
+++ testautomation/testautomation/selenium_test.cs
@@ -8,117 +8,27 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTests
+namespace SeleniumTests 
 {
     [TestFixture]
-    public class SeleniumTest
+    public class SeleniumTest :Scenario
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            //driver = new FirefoxDriver();
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
-            baseURL = "https://automatyzacja2016.wordpress.com/";
-            verificationErrors = new StringBuilder();
-           
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
-
+       
         [Test]
-        public void TheSeleniumTest()
+        public void shouldAddNewPost()
         {
+            LoginIn("szkolenieautomatyzacja", "QW12qw12");
 
-            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautomatyzacja2016.wordpress.com%2Fwp-admin%2F&reauth=1");
-            driver.FindElement(By.Id("user_login")).Clear();
-            driver.FindElement(By.Id("user_login")).SendKeys("szkolenieautomatyzacja");
-            driver.FindElement(By.Id("user_pass")).Clear();
-            driver.FindElement(By.Id("user_pass")).SendKeys("QW12qw12");
-            driver.FindElement(By.Id("wp-submit")).Click();
+            AddPost("test");
 
-            driver.FindElement(By.Id("wp-admin-bar-ab-new-post")).Click();
-            driver.FindElement(By.ClassName("editor-title__input")).SendKeys("test");
+            LogOut();
+            AssertPostExistsWithTitle("test");
 
-
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            wait.Until(ExpectedConditions.ElementToBeClickable(
-                By.ClassName("editor-ground-control__publish-button")));
-            driver.FindElement(By.ClassName("editor-ground-control__publish-button")).Click();
-            Thread.Sleep(2000);
-
-            wait.Until(d => d.FindElement(By.ClassName("masterbar__item-me"))).Click();
-            wait.Until(d => d.FindElement(By.ClassName("me-sidebar__signout-button"))).Click();
-            // driver.FindElement(By.ClassName("avatar-32")).Click();
-            // driver.FindElement(By.ClassName("me-sidebar__signout-button")).Click();
+        }
+        protected void AssertPostExistsWithTitle(String text)
+        {
             driver.Navigate().GoToUrl(baseURL);
-            Assert.NotNull(wait.Until<IWebElement>(d=>d.FindElement(By.LinkText("test")))); 
-
-        }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
+            Assert.NotNull(wait.Until<IWebElement>(d => d.FindElement(By.LinkText(text))));
         }
     }
 }
