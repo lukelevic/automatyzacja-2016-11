a37de2f262a01bc79144be1c91df406073fc1673
Michalgrc <michal.grcc@gmail.com>
2016-09-22 17:15:04 +0200

    new

---
+++ testautomation/testautomation/Scenario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    public class Scenario
+    {
+        protected IWebDriver driver;
+        protected StringBuilder verificationErrors;
+        protected string baseURL;
+        protected bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            // driver = new FirefoxDriver()
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            baseURL = "https://automatyzacja2016.wordpress.com/wp-admin";
+            verificationErrors = new StringBuilder();
+        }
+        protected void Open(String baseURL)
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+
+        protected void tryLogIn(String login, String password)
+        {
+            InsertText(By.Id("user_login"), login);
+            InsertText(By.Id("user_pass"), password);
+            click(By.Id("wp-submit"));
+        }
+
+        protected void click(By by)
+        {
+            driver.FindElement(by).Click();
+        }
+
+
+        protected void InsertText(By by, String text)
+        {
+            driver.FindElement(by).Clear();
+            driver.FindElement(by).SendKeys(text);
+        }
+
+        protected void WaitForElement(By by)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        protected void logOut()
+        {
+            click(By.Id("wp-admin-bar-my-account"));
+            driver.FindElement(By.CssSelector("button.ab-sign-out")).Click();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+    }
+}
--- testautomation/testautomation/SeleniumTest.cs
+++ testautomation/testautomation/SeleniumTest.cs
@@ -12,45 +12,44 @@ using OpenQA.Selenium.Chrome;
 namespace SeleniumTest
 {
     [TestFixture]
-    public class SeleniumTest
+    public class SeleniumTest : Scenario
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            // driver = new FirefoxDriver()
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            baseURL = "https://automatyzacja2016.wordpress.com/";
-            verificationErrors = new StringBuilder();
-        }
         [Test]
-        public void AddNewCommentTest()
+        public void ShouldLogIn()
         {
             // given
-            driver.Navigate().GoToUrl("https://automatyzacja2016.wordpress.com/wp-admin");
+            Open(baseURL);
+            tryLogIn("szkolenieautomatyzacja", "QW12qw12");
 
-            // when
-            driver.FindElement(By.Id("user_login")).Clear();
-            driver.FindElement(By.Id("user_login")).SendKeys("szkolenieautomatyzacja");
-            driver.FindElement(By.Id("user_pass")).Clear();
-            driver.FindElement(By.Id("user_pass")).SendKeys("QW12qw12");
-            driver.FindElement(By.Id("wp-submit")).Click();
+            //when
+            InsertComment();
 
-            // then
+            //then
+            logOut();
+            AssertCommentAdded();
+        }
 
-            driver.FindElement(By.XPath("//*[@id=\"menu-posts\"]/a")).Click();
-            driver.FindElement(By.XPath("//*[@id=\"wpbody-content\"]/div[3]/h1/a")).Click();
-            driver.FindElement(By.Name("post_title")).SendKeys("my Title");
-            driver.FindElement(By.Id("content")).SendKeys("my comment");
-            driver.FindElement(By.Id("publish")).Click();
-            driver.Navigate().GoToUrl(baseURL);
+
+       
+        private void InsertComment()
+        {
+            click(By.XPath("//*[@id=\"menu-posts\"]/a"));
+            click(By.XPath("//*[@id=\"wpbody-content\"]/div[3]/h1/a"));
+            InsertText(By.Name("post_title"), "my Title");
+            InsertText(By.Id("content"), "my comment");
+
+            WaitForElement(By.Id("publish"));
+            click(By.Id("publish"));
+
+        }
+
+        private void AssertCommentAdded()
+        {
+            Open("https://automatyzacja2016.wordpress.com");
             Assert.True(driver.PageSource.Contains("my Title"));
         }
+
+             
         
 
 
