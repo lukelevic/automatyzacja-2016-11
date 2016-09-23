88ec6abcfd6f60a4d2fae28ff6e4f2dca825d102
andrzejm <andrzejmorawski334@gmail.com>
2016-09-23 09:24:28 +0200

    update test

---
+++ testautomation/testautomation/Page.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    public class Page
+    {
+        public WebDriverWait wait;
+        public IWebDriver driver;
+        public string baseURL, baseURLAdmin;
+
+        public void PrepareTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            baseURL = "https://automatyzacja2016.wordpress.com";
+            baseURLAdmin = "https://automatyzacja2016.wordpress.com/wp-admin";
+            driver.Navigate().GoToUrl(baseURLAdmin);
+            Logowanie("szkolenieautomatyzacja", "QW12qw12");
+        }
+        protected void EndTest()
+        {
+
+        }
+        public void click(By by)
+        {
+            wait.Until<IWebElement>(d => d.FindElement(by)).Click();
+        }
+        protected void sendKeys(By by, string text)
+        {
+            wait.Until<IWebElement>(d => d.FindElement(by)).Click();
+            wait.Until<IWebElement>(d => d.FindElement(by)).SendKeys(text);
+        }
+        protected void Logowanie(string login, string pass)
+        {
+            sendKeys(By.Id("user_login"), login);
+            sendKeys(By.Id("user_pass"), pass);
+            click(By.Id("wp-submit"));
+        }
+        protected void Wylogowanie()
+        {
+            click(By.ClassName("avatar-32"));
+            click(By.ClassName("ab-sign-out"));
+        }
+        protected void SzukajWpisu(string SzukanyWpis)
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            Assert.NotNull(wait.Until<IWebElement>(d => d.FindElement(By.LinkText(SzukanyWpis))));
+        }
+        protected string GenerateRandomName(string name)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[16];
+            var random = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var tmp = new String(stringChars);
+            return name + tmp;
+        }
+    }
+}
--- testautomation/testautomation/SeleniuIDETest.cs
+++ testautomation/testautomation/SeleniuIDETest.cs
@@ -11,130 +11,44 @@ using OpenQA.Selenium.Support.UI;
 namespace SeleniumTests
 {
     [TestFixture]
-    public class SeleniuIDETest
+    public class SeleniuIDETest : Page
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL, baseURLAdmin;
-        private bool acceptNextAlert = true;
-        WebDriverWait wait;
-        string finalString;
+
 
         [SetUp]
         public void SetupTest()
         {
-            //driver = new FirefoxDriver();
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            baseURL = "https://automatyzacja2016.wordpress.com";
-            baseURLAdmin = "https://automatyzacja2016.wordpress.com/wp-admin";
-            verificationErrors = new StringBuilder();
-
-            finalString = GenerateRandomName("AM title: ");
+            PrepareTest();
         }
         
         [TearDown]
         public void TeardownTest()
         {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
+            driver.Quit();
         }
         
         [Test]
-        public void SholudAddNewPost()
+        public void ShouldAddNewPost()
         {
-            driver.Navigate().GoToUrl(baseURLAdmin);
-            Logowanie("szkolenieautomatyzacja", "QW12qw12");
-            AddPost("Teksat AM");
+            // GIVEN 
+            string finalString;
+            finalString = GenerateRandomName("AM title: ");
+
+            // THEN
+            AddPost(finalString);
             Wylogowanie();
+
+            // WHEN
             SzukajWpisu(finalString);
         }
 
-        private void Logowanie(string login, string pass)
+        protected void AddPost(string postText)
         {
-            driver.FindElement(By.Id("user_login")).Click();
-            driver.FindElement(By.Id("user_login")).Clear();
-            driver.FindElement(By.Id("user_login")).SendKeys(login);
-            driver.FindElement(By.Id("user_pass")).Clear();
-            driver.FindElement(By.Id("user_pass")).SendKeys(pass);
-            driver.FindElement(By.Id("wp-submit")).Click();
+            click(By.LinkText("Wpisy"));
+            click(By.LinkText("Dodaj nowy"));
+            sendKeys(By.Id("title"), postText);
+            click(By.Id("publish"));
         }
-        private void AddPost(string postText)
-        {
-            wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Wpisy"))).Click();
-            wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Dodaj nowy"))).Click();
-            wait.Until<IWebElement>(d => d.FindElement(By.Id("title"))).SendKeys(finalString);
-            wait.Until<IWebElement>(d => d.FindElement(By.Id("publish"))).Click();
-        }
-        private void Wylogowanie()
-        {
-            wait.Until<IWebElement>(d => d.FindElement(By. ClassName("avatar-32"))).Click();
-            wait.Until<IWebElement>(d => d.FindElement(By.ClassName("ab-sign-out"))).Click();
-        }
-        private void SzukajWpisu(string SzukanyWpis)
-        {
-            driver.Navigate().GoToUrl(baseURL);
-            Assert.NotNull(wait.Until<IWebElement>(d => d.FindElement(By.LinkText(SzukanyWpis))));
-        }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-        private string CloseAlertAndGetItsText() {
-            try {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert) {
-                    alert.Accept();
-                } else {
-                    alert.Dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
-        }
-        private string GenerateRandomName(string name)
-        {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var stringChars = new char[14];
-            var random = new Random();
 
-            for (int i = 0; i < 6; i++)
-            {
-                stringChars[i] = chars[random.Next(chars.Length)];
-            }
-
-            var tmp = new String(stringChars);
-            return name + tmp;
-        }
     }
 }
