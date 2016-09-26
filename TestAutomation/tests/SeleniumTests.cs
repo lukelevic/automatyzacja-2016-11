4c232693fb080d604f423cfaeabb5fbcf618479e
Rafał Markowicz <rafal.markowicz@codesprinters.com>
2016-09-26 11:42:38 +0200

    Commented out selenium grid related changes

--- TestAutomation/tests/SeleniumTests.cs
+++ TestAutomation/tests/SeleniumTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
+using OpenQA.Selenium.Remote;
 
 namespace TestAutomation.tests
 {
@@ -16,6 +17,9 @@ namespace TestAutomation.tests
         protected void setupTest()
         {
             driver = new ChromeDriver();
+            //DesiredCapabilities caps = new DesiredCapabilities();
+            //caps = DesiredCapabilities.Chrome();
+            //driver = new RemoteWebDriver(new Uri("http://localhost:4444/hub"), caps);
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
             driver.Manage().Window.Maximize();
             verificationErrors = new StringBuilder();
